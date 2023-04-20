1c14996ccba0fc9e2ae74bd5d02c2fc903a1623a
kchambit <kev132011@hotmail.com>
2023-04-20 13:07:23 -0500

    L-7: Advanced refactoring with rust modules and bevy plugins

--- bevy-ball-game/src/main.rs
+++ bevy-ball-game/src/main.rs
@@ -1,10 +1,12 @@
-pub mod components;
 pub mod events;
-pub mod resources;
 mod systems;
 
+pub mod enemy;
+mod player;
+pub mod score;
+mod star;
+
 use events::*;
-use resources::*;
 use systems::*;
 
 use bevy::prelude::*;
@@ -12,30 +14,13 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .init_resource::<Score>()
-        .init_resource::<HighScores>()
-        .init_resource::<StarSpawnTimer>()
-        .init_resource::<EnemySpawnTimer>()
         .add_event::<GameOver>()
+        .add_plugin(enemy::EnemyPlugin)
+        .add_plugin(player::PlayerPlugin)
+        .add_plugin(score::ScorePlugin)
+        .add_plugin(star::StarPlugin)
         .add_startup_system(spawn_camera)
-        .add_startup_system(spawn_player)
-        .add_startup_system(spawn_enemies)
-        .add_startup_system(spawn_stars)
-        .add_system(player_movement)
-        .add_system(confine_player_movement)
-        .add_system(enemy_movement)
-        .add_system(update_enemy_direction)
-        .add_system(confine_enemy_movement)
-        .add_system(enemy_hit_player)
-        .add_system(player_hit_star)
-        .add_system(update_score)
-        .add_system(tick_star_spawn_timer)
-        .add_system(spawn_stars_over_time)
-        .add_system(tick_enemy_spawn_timer)
-        .add_system(spawn_enemies_over_time)
         .add_system(exit_game)
         .add_system(handle_game_over)
-        .add_system(update_high_scores)
-        .add_system(high_scores_updated)
         .run();
 }
---
+++ bevy-ball-game/src/score/systems.rs
@@ -0,0 +1,29 @@
+use super::resources::*;
+use crate::events::GameOver;
+use bevy::prelude::*;
+
+pub fn update_score(score: Res<Score>) {
+    if score.is_changed() {
+        println!("Score: {}", score.value.to_string());
+    }
+}
+
+pub fn update_high_scores(
+    mut game_over_event_reader: EventReader<GameOver>,
+    mut high_scores: ResMut<HighScores>,
+) {
+    for event in game_over_event_reader.iter() {
+        high_scores.scores.push(("Player".to_string(), event.score));
+        high_scores.scores.sort();
+        high_scores.scores.reverse();
+        if high_scores.scores.len() > 10 {
+            high_scores.scores.pop();
+        }
+    }
+}
+
+pub fn high_scores_updated(high_scores: Res<HighScores>) {
+    if high_scores.is_changed() {
+        println!("High Scores: {:?}", high_scores);
+    }
+}
